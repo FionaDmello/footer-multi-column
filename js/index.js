a6695b1d9952092eed7cb75c05d531e6ebe8e16b
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-08-14 16:43:51 +0200

    added event listener to email form

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+const form = document.getElementById("newsletterInputForm");
+
+form.addEventListener("submit", (event) => {
+  const email = document.getElementById("email");
+  console.log("here", email.validity);
+
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+    email.reportValidity();
+  }
+});
